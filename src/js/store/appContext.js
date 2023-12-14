54901f88e34b70a2601b3916f2b494b08ef98be8
Hugh Anderson <144385898+HughAnderson23@users.noreply.github.com>
2023-12-14 05:32:24 +0000

    not sure what im doing

--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import getState from "./flux.js";
 
-// Don't change, here is where we initialize our context, by default it's just going to be null.
-export const Context = React.createContext(null);
+// Don't change, here is where we initialize our context, by default its just going to be Null.
+export const { Consumer, Provider } = React.createContext(null);
 
-// This function injects the global store to any view/component where you want to use it, we will inject the context to layout.js, you can see it here:
-// https://github.com/4GeeksAcademy/react-hello-webapp/blob/master/src/js/layout.js#L35
+// This function injects the global store to any view/component where you want to use it, we will inject the context to Layout.jsx, you can see it here:
+// https://github.com/4GeeksAcademy/react-hello-webapp/blob/master/src/js/layout.jsx#L35
 const injectContext = PassedComponent => {
 	const StoreWrapper = props => {
 		//this will be passed as the contenxt value
@@ -24,22 +24,32 @@ const injectContext = PassedComponent => {
 		useEffect(() => {
 			/**
 			 * EDIT THIS!
-			 * This function is the equivalent to "window.onLoad", it only runs once on the entire application lifetime
-			 * you should do your ajax requests or fetch api requests here. Do not use setState() to save data in the
-			 * store, instead use actions, like this:
+			 * This function is the equivalent to "window.onLoad", it only run once on the entire application lifetime
+			 * you should do your ajax requests or fetch api requests here
 			 *
-			 * state.actions.loadSomeData(); <---- calling this function from the flux.js actions
+			 * state.loadSomeData(); <---- calling this function from the flux.js actions
 			 *
 			 **/
+			fetch("https://playground.4geeks.com/apis/fake/contact")
+				.then(response => response.json())
+				.then(data => {
+					let { store, actions } = state;
+					// setState(store.agenda);
+					//setState({ store: { ...state.store, agenda: data } });
+					setState({ store: { ...store, apiContacts: data }, actions });
+					console.log("FETCH", state);
+					// setState({ store: { ...store, agenda: data } });
+				})
+				.then(() => console.log("THEN", state));
 		}, []);
 
-		// The initial value for the context is not null anymore, but the current state of this component,
-		// the context will now have a getStore, getActions and setStore functions available, because they were declared
+		// the initial value for the context its not null anymore, but the current state of this component,
+		// the context will have a getStore and setStore functions available then, because they were declared
 		// on the state of this component
 		return (
-			<Context.Provider value={state}>
+			<Provider value={state}>
 				<PassedComponent {...props} />
-			</Context.Provider>
+			</Provider>
 		);
 	};
 	return StoreWrapper;
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,42 +1,82 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
+			apiContacts: [],
+			localContacts: [
 				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
+					name: "Paolo",
+					email: "pluco@gmail.com",
+					address: "123 Lincoln rd",
+					phone: "123456"
 				}
 			]
 		},
+
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			addANewContact: (name, email, address, phone, saveLocation, history) => {
+				let store = getStore();
+				console.log("CLICK", store);
+				// This method will receive name, address, phone and email from addContact view
+				// and it will post to the backend or to the store
+				saveLocation === "store"
+					? setStore({
+							localContacts: store.localContacts.concat({
+								name: name,
+								email: email,
+								address: address,
+								phone: phone
+							})
+					  })
+					: fetch("https://playground.4geeks.com/apis/fake/contact", {
+							method: "POST",
+							headers: { "Content-Type": "application/json" },
+							body: JSON.stringify({
+								name: name,
+								email: email,
+								address: address,
+								phone: phone
+							})
+					  })
+							.then(() => {
+								fetch("https://playground.4geeks.com/apis/fake/contact")
+									.then(response => response.json())
+									.then(data => {
+										setStore({ apiContacts: data });
+									});
+							})
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+							.then(() => history.push("/contacts/api"));
+			},
+			editContact: (name, email, address, phone, saveLocation, index) => {
+				let store = getStore();
+				let updated_store = store.localContacts
+					.slice(0, index)
+					.concat({
+						...store.localContacts[index],
+						name: name,
+						email: email,
+						address: address,
+						phone: phone
+					})
+					.concat(store.localContacts.slice(index + 1));
+				console.log("Upd", updated_store);
+				setStore({ localContacts: updated_store });
+				saveLocation === "store"
+					? setStore({
+							localContacts: store.localContacts.splice(index, 1, {
+								...store[index],
+								name: name,
+								email: email,
+								address: address,
+								phone: phone
+							})
+					  })
+					: console.log("api:", name, email, address, phone).then(() => history.push("/"));
+			},
+			deleteContact: index => {
+				let store = getStore();
+				console.log("index", index);
+				setStore({ localContacts: store.localContacts.filter((item, index) => index !== index) });
 			}
 		}
 	};
